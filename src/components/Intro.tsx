109d95579b5cdf7137415dff8c0a6bd19f10a9c5
Florian Van Belleghem <fvanbelleghem@icloud.com>
2023-04-29 16:10:11 +0200

    Create Intro

---
+++ src/components/Intro.tsx
@@ -0,0 +1,26 @@
+import useStore from "../hooks/useStore";
+import { AiOutlineArrowRight } from "react-icons/ai";
+import "./Intro.module.css";
+
+const Intro = () => {
+  const { setIsIntro } = useStore();
+
+  return (
+    <section>
+      <h1>Kangaroo</h1>
+
+      <p>
+        Designed for the Hall of the Chandigarh General Hospital and present in
+        many private homes, the chair is known for the “Z” shape of its sides,
+        made up of three flush elements.
+      </p>
+
+      <button onClick={() => setIsIntro(false)}>
+        Customize
+        <AiOutlineArrowRight />
+      </button>
+    </section>
+  );
+};
+
+export default Intro;
