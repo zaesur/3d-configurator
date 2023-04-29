1c0e7a516cbc578ab6155ef1e1fbc1052f5ca330
Florian Van Belleghem <fvanbelleghem@icloud.com>
2023-04-29 16:59:12 +0200

    Give Chair ref

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./index.css";
 import { Canvas } from "@react-three/fiber";
-import { Chair } from "./components/models/Chair";
+import Chair from "./components/models/Chair";
 import Overlay from "./components/overlay/Overlay";
 import { Stage } from "@react-three/drei";
 
