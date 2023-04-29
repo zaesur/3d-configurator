595553ac523d5da03bc495e0a4761d5950c94d89
Florian Van Belleghem <fvanbelleghem@icloud.com>
2023-04-29 16:59:12 +0200

    Give Chair ref

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
 import "./index.css";
 import { Canvas } from "@react-three/fiber";
-import { Chair } from "./components/models/Chair";
+import Chair from "./components/models/Chair";
 import Overlay from "./components/overlay/Overlay";
-import { Stage } from "@react-three/drei";
+import { Environment } from "@react-three/drei";
 
 const App = () => {
   return (
     <>
       <Canvas dpr={[1, 2]}>
-        <Stage
-          adjustCamera={2}
-          intensity={0.5}
-          shadows="contact"
-          environment="city"
-        >
-          <Chair rotation={[0, (Math.PI / 4) * -3, 0]} />
-        </Stage>
+        <ambientLight intensity={0.5} />
+        <Chair rotation={[0, (Math.PI / 4) * -3, 0]} />
+        <Environment preset="city" />
       </Canvas>
       <Overlay />
     </>
