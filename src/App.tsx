4ba428cfdb29054beee0018b2afc6d50251d43b4
Florian Van Belleghem <fvanbelleghem@icloud.com>
2023-04-30 12:28:21 +0200

    Disable pan and zoom

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,21 @@ import { Canvas } from "@react-three/fiber";
 import Chair from "./components/models/Chair";
 import Overlay from "./components/overlay/Overlay";
 import { Environment, OrbitControls } from "@react-three/drei";
+import useStore from "./hooks/useStore";
 
 const App = () => {
+  const { isIntro } = useStore();
+
   return (
     <>
       <Overlay />
       <Canvas dpr={[1, 2]}>
-        <OrbitControls />
+        <OrbitControls
+          enablePan={false}
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          enabled={!isIntro}
+        />
         <ambientLight intensity={0.5} />
         <Chair rotation={[0, (Math.PI / 4) * -3, 0]} />
         <Environment preset="city" />
