acdf2ce79cd069f2e39d51f0895552f64628fed8
Florian Van Belleghem <fvanbelleghem@icloud.com>
2023-04-30 02:07:54 +0200

    Restore OrbitControls

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import "./index.css";
 import { Canvas } from "@react-three/fiber";
 import Chair from "./components/models/Chair";
 import Overlay from "./components/overlay/Overlay";
-import { Environment } from "@react-three/drei";
+import { Environment, OrbitControls } from "@react-three/drei";
 
 const App = () => {
   return (
     <>
       <Overlay />
       <Canvas dpr={[1, 2]}>
+        <OrbitControls />
         <ambientLight intensity={0.5} />
         <Chair rotation={[0, (Math.PI / 4) * -3, 0]} />
         <Environment preset="city" />
