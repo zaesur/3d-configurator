f3e0980f1dab9879cc5dc860f640172c50255ed2
Florian Van Belleghem <fvanbelleghem@icloud.com>
2023-04-30 21:33:22 +0200

    Improve CSS

--- src/components/configurator/Configurator.tsx
+++ src/components/configurator/Configurator.tsx
@@ -1,8 +1,9 @@
 import useStore from "../../hooks/useStore";
 import TextureSelect from "../texture_selector/TextureSelector";
+import { Config } from "../overlay/Overlay";
 import { motion } from "framer-motion";
 import { FunctionComponent } from "react";
-import { Config } from "../overlay/Overlay";
+import "./Configurator.module.css";
 
 interface ConfiguratorProps {
   config: Config;
