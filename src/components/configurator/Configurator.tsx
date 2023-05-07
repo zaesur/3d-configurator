5b493a745d21ca657b5ae5f046039393ea9eed96
Florian Van Belleghem <fvanbelleghem@icloud.com>
2023-05-07 16:44:29 +0200

    Introduce class names in modules

--- src/components/configurator/Configurator.tsx
+++ src/components/configurator/Configurator.tsx
@@ -2,12 +2,13 @@ import useStore from "../../hooks/useStore";
 import TextureSelect from "../texture_selector/TextureSelector";
 import Button from "../button/Button";
 import { FunctionComponent } from "react";
+import styles from "./Configurator.module.css";
 
 const Configurator: FunctionComponent = () => {
   const { setIsIntro } = useStore();
 
   return (
-    <article>
+    <article className={styles.article}>
       <Button label="Exit" onClick={() => setIsIntro(true)} />
       <TextureSelect />
     </article>
--- src/components/intro/Intro.tsx
+++ src/components/intro/Intro.tsx
@@ -7,18 +7,16 @@ const Intro: FunctionComponent = () => {
   const { setIsIntro } = useStore();
 
   return (
-    <article>
+    <article className={styles.article}>
       <h1>Kangaroo Armchair</h1>
 
-      <div className={styles.container}>
-        <h2>An hommage to Pierre Jeanneret</h2>
-        <p>
-          Designed for the Hall of the Chandigarh General Hospital and present
-          in many private homes, the chair is known for the “Z” shape of its
-          sides, made up of three flush elements.
-        </p>
-        <Button label="Customize" onClick={() => setIsIntro(false)} />
-      </div>
+      <h2>An hommage to Pierre Jeanneret</h2>
+      <p>
+        Designed for the Hall of the Chandigarh General Hospital and present in
+        many private homes, the chair is known for the “Z” shape of its sides,
+        made up of three flush elements.
+      </p>
+      <Button label="Customize" onClick={() => setIsIntro(false)} />
     </article>
   );
 };
